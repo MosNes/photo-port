5f4dd84634219506f2f36177ff2151b3571b5f07
MosNes <7hekarl@gmail.com>
2022-09-11 20:51:17 -0400

    added handleChange to sync input data with state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
 
@@ -38,6 +39,7 @@ function App() {
 		currentCategory={currentCategory}
 	  ></Nav>
 			<main>
+				<ContactForm></ContactForm>
 				{/* pass current category into gallery */}
 				<Gallery currentCategory={currentCategory}></Gallery>
 				<About></About>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ContactForm() {
+
+    //sets initial default state of input fields to empty values
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    //destructure formState into 3 variables
+    const { name, email, message } = formState;
+
+    //create changeHandler to sync the values for the input fields to the current state
+    function handleChange(e) {
+        //use spread operator to retain all key-value pairs, otherwise formState would be overwritten to contain only the single key-value defined here
+        // [e.target.name] dynamically generates the property name and matching value
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
     return (
         <section>
             <h1>Contact Me</h1>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email Address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
                 </div>
                 <button type="submit">Submit</button>
             </form>
